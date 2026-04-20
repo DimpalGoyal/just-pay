53c655f4bf43d48f2deabfba24f763bb0e49f425
Dimpal Goyal <dimpalgoyal129@gmail.com>
2026-04-20 14:00:53 +0000

    added account-schema in db.js and refined user schema

--- backend/db.js
+++ backend/db.js
@@ -7,13 +7,45 @@ mongoose.connect(process.env.MONGODB_URL)
 
 // hashing password before storing 
 const userSchema = mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    password: String,
-    username: String
+    firstName: {
+        type: String,
+        required: true,
+        maxLength: 10,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        maxLength: 10,
+    },
+    password: {
+        type: String,
+        required: true,
+        maxLength: 10,
+    },
+    username: {
+        unique: true,
+        type: String,
+        required: true,
+        maxLength: 20,
+    },
+})
+
+const accountSchema = new mongoose.schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    balance:{
+        type: Number,
+        required: true, 
+    }
 })
 
 const User = mongoose.model('User', userSchema)
+const Account = mongoose.model('Account', accountSchema)
+
 module.exports = {
-    User
+    User,
+    Account
 }
